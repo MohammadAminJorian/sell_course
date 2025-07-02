0250a1a2f3401820cd4cb56d5f150807df4156c2
mohammad amin jorian <130844153+MohammadAminJorian@users.noreply.github.com>
2025-07-02 20:02:46 +0330

    Update settings.py

--- djangoo/djnagoo/settings.py
+++ djangoo/djnagoo/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'coodee129@gmail.com'
-EMAIL_HOST_PASSWORD = 'jynwwgtxpdaklhei'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 
